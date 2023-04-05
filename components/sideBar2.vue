1ee36d9b9bae4bfe3bacd7c080434d4710630a71
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-05 13:29:47 -0400

    1 file modified

--- components/sideBar2.vue
+++ components/sideBar2.vue
@@ -3,7 +3,7 @@
     <TransitionRoot as="template" :show="sidebarOpen">
       <Dialog
         as="div"
-        class="relative z-50 lg:hidden"
+        class="relative z-40 lg:hidden"
         @close="sidebarOpen = false"
       >
         <TransitionChild
@@ -15,7 +15,7 @@
           leave-from="opacity-100"
           leave-to="opacity-0"
         >
-          <div class="fixed inset-0 bg-gray-900/80" />
+          <div class="static fixed inset-0 bg-gray-800" />
         </TransitionChild>
 
         <div class="fixed inset-0 flex">
@@ -361,16 +361,30 @@
         </nav>
       </div>
     </div>
-        <div class="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
-      <button type="button" class="-m-2.5 p-2.5 text-gray-400 lg:hidden" @click="sidebarOpen = true">
+    <div
+      class="
+        sticky
+        top-0
+        z-30
+        flex
+        items-center
+        gap-x-6
+        bg-gray-900
+        px-4
+        py-4
+        shadow-sm
+        sm:px-6
+        lg:hidden
+      "
+    >
+      <button
+        type="button"
+        class="-m-2.5 p-2.5 text-gray-400 lg:hidden"
+        @click="sidebarOpen = true"
+      >
         <span class="sr-only">Open sidebar</span>
         <Bars3Icon class="h-6 w-6" aria-hidden="true" />
       </button>
-      <div class="flex-1 text-sm font-semibold leading-6 text-white">Dashboard</div>
-      <a href="#">
-        <span class="sr-only">Your profile</span>
-        <img class="h-8 w-8 rounded-full bg-gray-800" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" />
-      </a>
     </div>
   </div>
 </template>
