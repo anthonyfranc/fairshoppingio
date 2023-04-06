d0296f388b3291b39bf8c5f5de85a89eb4a06906
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-06 11:45:41 -0400

    1 file modified

--- components/sideBar2.vue
+++ components/sideBar2.vue
@@ -66,7 +66,7 @@
                 "
               >
                 <div class="flex h-16 shrink-0 items-center">
-                  <NuxtLink to="/" class="flex items-center pl-2.5 mb-5">
+                  <NuxtLink to="/" class="flex items-center mb-5 mt-[2.9em]">
                     <!--Logo-->
                     <div class="logoSlide">
                       <span
@@ -94,63 +94,162 @@
                 <nav class="flex flex-1 flex-col">
                   <ul role="list" class="flex flex-1 flex-col gap-y-7">
                     <li>
-                      <ul role="list" class="-mx-2 space-y-1">
-                        <li v-for="item in navigation" :key="item.name">
-                          <a
-                            :href="item.href"
-                            :class="[
-                              item.current
-                                ? 'bg-gray-800 text-white'
-                                : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
-                            ]"
-                          >
-                            <component
-                              :is="item.icon"
-                              class="h-6 w-6 shrink-0"
-                              aria-hidden="true"
-                            />
-                            {{ item.name }}
-                          </a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
                       <div
                         class="text-xs font-semibold leading-6 text-gray-400"
                       >
-                        Your teams
+                        Product Category
                       </div>
                       <ul role="list" class="-mx-2 mt-2 space-y-1">
-                        <li v-for="team in teams" :key="team.name">
+                        <li>
                           <a
-                            :href="team.href"
-                            :class="[
-                              team.current
-                                ? 'bg-gray-800 text-white'
-                                : 'text-gray-400 hover:text-white hover:bg-gray-800',
-                              'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
-                            ]"
+                            href="team.href"
+                            class="
+                              hover:bg-gray-800
+                              text-white text-gray-400
+                              hover:text-white hover:bg-gray-800
+                              group
+                              flex
+                              gap-x-3
+                              rounded-md
+                              p-3
+                              text-sm
+                              leading-3
+                              font-semibold
+                            "
                           >
                             <span
                               class="
                                 flex
-                                h-6
-                                w-6
+                                h-3
+                                w-3
                                 shrink-0
                                 items-center
                                 justify-center
-                                rounded-lg
-                                border border-gray-700
-                                bg-gray-800
+                                rounded-full
+                                ring-1 ring-white
+                                bg-amber-500
                                 text-[0.625rem]
                                 font-medium
                                 text-gray-400
                                 group-hover:text-white
                               "
-                              >{{ team.initial }}</span
                             >
-                            <span class="truncate">{{ team.name }}</span>
+                            </span>
+                            <span class="truncate">Electronics</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="team.href"
+                            class="
+                              hover:bg-gray-800
+                              text-white text-gray-400
+                              hover:text-white hover:bg-gray-800
+                              group
+                              flex
+                              gap-x-3
+                              rounded-md
+                              p-3
+                              text-sm
+                              leading-3
+                              font-semibold
+                            "
+                          >
+                            <span
+                              class="
+                                flex
+                                h-3
+                                w-3
+                                shrink-0
+                                items-center
+                                justify-center
+                                rounded-full
+                                ring-1 ring-white
+                                bg-violet-600
+                                text-[0.625rem]
+                                font-medium
+                                text-gray-400
+                                group-hover:text-white
+                              "
+                            >
+                            </span>
+                            <span class="truncate">Video Games</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="team.href"
+                            class="
+                              hover:bg-gray-800
+                              text-white text-gray-400
+                              hover:text-white hover:bg-gray-800
+                              group
+                              flex
+                              gap-x-3
+                              rounded-md
+                              p-3
+                              text-sm
+                              leading-3
+                              font-semibold
+                            "
+                          >
+                            <span
+                              class="
+                                flex
+                                h-3
+                                w-3
+                                shrink-0
+                                items-center
+                                justify-center
+                                rounded-full
+                                ring-1 ring-white
+                                bg-cyan-600
+                                text-[0.625rem]
+                                font-medium
+                                text-gray-400
+                                group-hover:text-white
+                              "
+                            >
+                            </span>
+                            <span class="truncate">Entertainment</span>
+                          </a>
+                        </li>
+                        <li>
+                          <a
+                            href="team.href"
+                            class="
+                              hover:bg-gray-800
+                              text-white text-gray-400
+                              hover:text-white hover:bg-gray-800
+                              group
+                              flex
+                              gap-x-3
+                              rounded-md
+                              p-3
+                              text-sm
+                              leading-3
+                              font-semibold
+                            "
+                          >
+                            <span
+                              class="
+                                flex
+                                h-3
+                                w-3
+                                shrink-0
+                                items-center
+                                justify-center
+                                rounded-full
+                                ring-1 ring-white
+                                bg-rose-300
+                                text-[0.625rem]
+                                font-medium
+                                text-gray-400
+                                group-hover:text-white
+                              "
+                            >
+                            </span>
+                            <span class="truncate">Smart Home</span>
                           </a>
                         </li>
                       </ul>
@@ -166,7 +265,7 @@
 
     <!-- Static sidebar for desktop -->
     <div
-      class="hidden lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:w-72 lg:flex-col"
+      class="hidden lg:fixed lg:inset-y-0 lg:z-40 lg:flex lg:w-64 lg:flex-col"
     >
       <!-- Sidebar component, swap this element with another sidebar if you like -->
       <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
