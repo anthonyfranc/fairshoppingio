45bb23780cf15d246ec39d662c9d712b3f295f3a
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-06 21:46:21 -0400

    1 file modified, 1 added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,4 +30,26 @@ export default defineNuxtConfig({
       },
     },
   },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {},
+
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000,
+  },
 });
