b7a1fa80a1c64c0ddc084ede7d7d56888bf61c8e
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-08 17:05:34 -0400

    2 files modified

--- composables/useState.ts
+++ composables/useState.ts
@@ -1,1 +1,6 @@
-export const useTicker = () => useState<any>('ticker', () => new Date().getTime())
+export const useTicker = () =>
+  useState<any>('ticker', () => new Date().getTime());
+
+setInterval(function () {
+  
+}, 1000);
