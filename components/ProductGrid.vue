7215a1650295a1aa0184476bb7d824f8a2446e83
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-08 15:41:51 -0400

    2 files modified

--- components/ProductGrid.vue
+++ components/ProductGrid.vue
@@ -195,6 +195,39 @@
             </button>
           </div>
           <div
+            class="
+              absolute
+              z-10
+              top-3.5
+              left-3
+              bg-blue-100
+              text-blue-800 text-xs
+              font-semibold
+              mr-2
+              px-1.5
+              py-0.5
+              rounded
+              dark:bg-blue-200 dark:text-blue-800
+              flex
+            "
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-[13px] h-[13px] mt-[0.9px] mr-[0.7px]"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>
+            {{ relativeDate(ProductData.last_updated) }} ago
+          </div>
+          <div
             role="button"
             class="relative"
             @click="
