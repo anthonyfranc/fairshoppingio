ca52d1bfba187519fe28636abc4bd03cb97f6dd2
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-08 17:05:53 -0400

    1 file modified

--- composables/useDateHelpers.ts
+++ composables/useDateHelpers.ts
@@ -1,7 +1,6 @@
-import { formatDistance } from "date-fns";
+import { formatDistance } from 'date-fns';
 
 export const relativeDate = (date: string) => {
   console.log('date:', date);
-  const ticker = useTicker();
   return formatDistance(new Date(date), ticker.value);
 };
