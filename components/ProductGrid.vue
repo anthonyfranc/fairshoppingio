445b85fad93fde0631005ac9f3834c69da1c9a4a
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-08 15:43:29 -0400

    1 file modified

--- components/ProductGrid.vue
+++ components/ProductGrid.vue
@@ -217,7 +217,7 @@
               viewBox="0 0 24 24"
               stroke-width="1.5"
               stroke="currentColor"
-              class="w-[13px] h-[13px] mt-[0.9px] mr-[0.7px]"
+              class="w-[13px] h-[13px] mt-[0.9px] mr-[3px]"
             >
               <path
                 stroke-linecap="round"
@@ -225,7 +225,7 @@
                 d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
               />
             </svg>
-            {{ relativeDate(ProductData.last_updated) }} ago
+            {{ relativeDate(ProductData.last_updated).replace('about','') }} ago
           </div>
           <div
             role="button"
