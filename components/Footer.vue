db090c8e9f8eeec1a4a5f422098bcb26a49778fa
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-20 10:56:59 -0400

    1 file modified

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="bg-white dark:bg-gray-900 shadow-inner">
+  <footer class="bg-white dark:bg-gray-900 border-t-2 border-slate-200 border-opacity-50">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <div class="md:flex md:justify-between">
         <div class="mb-6 md:mb-0">
@@ -28,7 +28,7 @@
             </div>
           </NuxtLink>
         </div>
-        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+        <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-2">
           <div>
             <h2
               class="
@@ -40,20 +40,8 @@
                 dark:text-white
               "
             >
-              Resources
+              About Us
             </h2>
-            <ul class="text-gray-600 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="https://flowbite.com/" class="hover:underline"
-                  >Flowbite</a
-                >
-              </li>
-              <li>
-                <a href="https://tailwindcss.com/" class="hover:underline"
-                  >Tailwind CSS</a
-                >
-              </li>
-            </ul>
           </div>
           <div>
             <h2
@@ -66,44 +54,8 @@
                 dark:text-white
               "
             >
-              Follow us
+              About Us
             </h2>
-            <ul class="text-gray-600 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a
-                  href="https://github.com/themesberg/flowbite"
-                  class="hover:underline"
-                  >Github</a
-                >
-              </li>
-              <li>
-                <a href="https://discord.gg/4eeurUVvTy" class="hover:underline"
-                  >Discord</a
-                >
-              </li>
-            </ul>
-          </div>
-          <div>
-            <h2
-              class="
-                mb-6
-                text-sm
-                font-semibold
-                text-gray-900
-                uppercase
-                dark:text-white
-              "
-            >
-              Legal
-            </h2>
-            <ul class="text-gray-600 dark:text-gray-400 font-medium">
-              <li class="mb-4">
-                <a href="#" class="hover:underline">Privacy Policy</a>
-              </li>
-              <li>
-                <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-              </li>
-            </ul>
           </div>
         </div>
       </div>
