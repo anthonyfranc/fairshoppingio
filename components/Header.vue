20ca6a406d4b713933be7d64358ac61f45140881
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-26 13:02:27 -0400

    1 file modified

--- components/Header.vue
+++ components/Header.vue
@@ -50,7 +50,6 @@
               dark:text-gray-400 dark:hover:text-gray-500
             "
             href="#"
-            data-hs-theme-click-value="dark"
           >
             <svg
               class="w-4 h-4"
@@ -79,7 +78,6 @@
               dark:text-gray-400 dark:hover:text-gray-500
             "
             href="#"
-            data-hs-theme-click-value="light"
           >
             <svg
               class="w-4 h-4"
@@ -454,6 +452,7 @@
   </div>
 </template>
 <style type="css">
+@import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@700&display=swap');
 .logoSlide {
   animation: slideInLogo 1s ease 0s 1 normal none;
 }
@@ -470,107 +469,3 @@
   }
 }
 </style>
-<style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Urbanist:wght@700&display=swap');
-</style>
-<script setup lang="ts">
-const HSThemeAppearance = {
-  init() {
-    const defaultTheme = 'default';
-    let theme = localStorage.getItem('hs_theme') || defaultTheme;
-
-    if (document.querySelector('html').classList.contains('dark')) return;
-    this.setAppearance(theme);
-  },
-  _resetStylesOnLoad() {
-    const $resetStyles = document.createElement('style');
-    $resetStyles.innerText = `*{transition: unset !important;}`;
-    $resetStyles.setAttribute('data-hs-appearance-onload-styles', '');
-    document.head.appendChild($resetStyles);
-    return $resetStyles;
-  },
-  setAppearance(theme, saveInStore = true, dispatchEvent = true) {
-    const $resetStylesEl = this._resetStylesOnLoad();
-
-    if (saveInStore) {
-      localStorage.setItem('hs_theme', theme);
-    }
-
-    if (theme === 'auto') {
-      theme = window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'default';
-    }
-
-    document.querySelector('html').classList.remove('dark');
-    document.querySelector('html').classList.remove('default');
-    document.querySelector('html').classList.remove('auto');
-
-    document.querySelector('html').classList.add(this.getOriginalAppearance());
-
-    setTimeout(() => {
-      $resetStylesEl.remove();
-    });
-
-    if (dispatchEvent) {
-      window.dispatchEvent(
-        new CustomEvent('on-hs-appearance-change', { detail: theme })
-      );
-    }
-  },
-  getAppearance() {
-    let theme = this.getOriginalAppearance();
-    if (theme === 'auto') {
-      theme = window.matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'dark'
-        : 'default';
-    }
-    return theme;
-  },
-  getOriginalAppearance() {
-    const defaultTheme = 'default';
-    return localStorage.getItem('hs_theme') || defaultTheme;
-  },
-};
-HSThemeAppearance.init();
-
-window
-  .matchMedia('(prefers-color-scheme: dark)')
-  .addEventListener('change', (e) => {
-    if (HSThemeAppearance.getOriginalAppearance() === 'auto') {
-      HSThemeAppearance.setAppearance('auto', false);
-    }
-  });
-
-window.addEventListener('load', () => {
-  const $clickableThemes = document.querySelectorAll(
-    '[data-hs-theme-click-value]'
-  );
-  const $switchableThemes = document.querySelectorAll('[data-hs-theme-switch]');
-
-  $clickableThemes.forEach(($item) => {
-    $item.addEventListener('click', () =>
-      HSThemeAppearance.setAppearance(
-        $item.getAttribute('data-hs-theme-click-value'),
-        true,
-        $item
-      )
-    );
-  });
-
-  $switchableThemes.forEach(($item) => {
-    $item.addEventListener('change', (e) => {
-      HSThemeAppearance.setAppearance(e.target.checked ? 'dark' : 'default');
-    });
-
-    $item.checked = HSThemeAppearance.getAppearance() === 'dark';
-  });
-
-  window.addEventListener('on-hs-appearance-change', (e) => {
-    $switchableThemes.forEach(($item) => {
-      $item.checked = e.detail === 'dark';
-    });
-  });
-});
-</script>
- 
