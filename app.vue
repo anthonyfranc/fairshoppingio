97c702afe58ccd74f7b99275fd4244ad6acb759a
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-04-30 15:59:34 -0400

    1 file modified

--- app.vue
+++ app.vue
@@ -10,12 +10,10 @@ onMounted(() => {
   //we prefer light mode, but users are able to switch. Since we use SSR one reload the selected user color will still exist.
   colorMode.preference = 'light';
   //use first time access cookie to launch welcome modal.
-  const load = useCookie('isNewVisit', {
-    sameSite: true,
-    //Chrome Does not allow anything longer than 400 days
-    maxAge: '1707109200',
-    default: 1,
+  const firstVisit = useCookie('isFirstVisit', {
+    default: () => ({ value: 1 }),
     watch: true,
-  })
+    maxAge: '1707109200',
+  });
 });
 </script>
