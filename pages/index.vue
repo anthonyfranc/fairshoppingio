ef40d78b44d508fa3cedbc6284c50d6cc6b60bfa
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-05-01 16:24:19 +0000

    Fix offcanvas Mobile

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <meta name="viewport" content= "width=device-width, initial-scale=1.0">
 <template>
 <Header />
-<div id="hs-overlay-right" class="hs-overlay hs-overlay-open:translate-x-0 hidden translate-x-full fixed top-0 right-0 transition-all duration-300 transform h-full max-w-sm w-full w-full z-[60] bg-white border-l hidden" tabindex="-1">
+<div id="hs-overlay-right" class="hs-overlay hs-overlay-open:translate-x-0 hidden translate-x-full fixed top-0 right-0 transition-all duration-300 transform h-full max-w-sm w-full z-[60] bg-white border-l hidden" tabindex="-1"
+style="overflow:scroll;">
    <LoadItem/>
 </div>
   <!--End of Drawer-->
@@ -15,15 +16,8 @@
 const appLoaded = ref(false);
 // initialize components based on data attribute selectors
 onMounted(() => {
-  document.documentElement.style.setProperty("--vh", window.innerHeight * 0.01 + 'px');
   appLoaded.value = true;
   let startimg = new Image();
   startimg.src = 'https://via.placeholder.com/160x160/d1d5db/d1d5db';
 });
 </script>
-<style type="css">
-.h-screen {
-    height: 100vh; /* Fallback for browsers that do not support Custom Properties */
-    height: calc(var(--vh, 1vh) * 100);
-}
-</style>
