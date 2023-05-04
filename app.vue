0365b4a4a5ea7282719265d9d814c4f6f8216f6a
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-05-03 22:46:50 -0400

    1 file modified, 1 added, 1 deleted

--- app.vue
+++ app.vue
@@ -10,4 +10,17 @@ onMounted(() => {
   //we prefer light mode, but users are able to switch. Since we use SSR one reload the selected user color will still exist.
   colorMode.preference = 'light';
 });
+const route = useRoute();
+watch(
+  () => route.params,
+  async () => {
+    if (route.params.id) {
+      //we are hiding the backdrop on /products/:id
+      $('.hs-overlay-backdrop').hide();
+    } else {
+      //we are going to show backdrop if we go back to main page, since we use keep-alive this will work.
+      $('.hs-overlay-backdrop').show();
+    }
+  }
+);
 </script>
