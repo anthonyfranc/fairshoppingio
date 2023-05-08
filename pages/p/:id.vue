d1ba925aa98327adb44c5da34d7dce121155fe82
anthonyfranc <anthonyfrancisvasquez@gmail.com>
2023-05-08 14:42:31 -0400

    1 file modified

--- pages/p/:id.vue
+++ pages/p/:id.vue
@@ -366,8 +366,108 @@
           </div>
         </div>
       </div>
+      <div class="border-b border-gray-200 dark:border-gray-700">
+        <nav class="flex space-x-2" aria-label="Tabs" role="tablist">
+          <button
+            type="button"
+            class="
+              hs-tab-active:font-semibold
+              hs-tab-active:border-blue-600
+              hs-tab-active:text-blue-600
+              py-4
+              px-1
+              inline-flex
+              items-center
+              gap-2
+              border-b-[3px] border-transparent
+              text-sm
+              whitespace-nowrap
+              text-gray-500
+              hover:text-blue-600
+              active
+            "
+            id="tabs-with-underline-item-1"
+            data-hs-tab="#tabs-with-underline-1"
+            aria-controls="tabs-with-underline-1"
+            role="tab"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+              />
+            </svg>
+
+            Discussion
+          </button>
+          <button
+            type="button"
+            class="
+              hs-tab-active:font-semibold
+              hs-tab-active:border-blue-600
+              hs-tab-active:text-blue-600
+              py-4
+              px-1
+              inline-flex
+              items-center
+              gap-2
+              border-b-[3px] border-transparent
+              text-sm
+              whitespace-nowrap
+              text-gray-500
+              hover:text-blue-600
+            "
+            id="tabs-with-underline-item-2"
+            data-hs-tab="#tabs-with-underline-2"
+            aria-controls="tabs-with-underline-2"
+            role="tab"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-5 h-5"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"
+              />
+            </svg>
+
+            Logs
+          </button>
+        </nav>
+      </div>
+
       <div class="bg-slate-50 rounded p-5">
-        <div id="replybox"></div>
+        <div
+          id="tabs-with-underline-1"
+          role="tabpanel"
+          aria-labelledby="tabs-with-underline-item-1"
+        >
+          <div id="replybox"></div>
+        </div>
+        <div
+          id="tabs-with-underline-2"
+          class="hidden"
+          role="tabpanel"
+          aria-labelledby="tabs-with-underline-item-2"
+        >
+          <!-- Table-->
+          <div id="myTable"></div>
+          <!-- Table-->
+        </div>
       </div>
     </div>
   </div>
@@ -400,6 +500,7 @@ function toggleTabs(tabNumber) {
 }
 
 openTab.value = data[0]['images'][0]['id'];
+
 onMounted(() => {
   window.replybox = {
     site: 'POBODlPRVa',
@@ -407,7 +508,7 @@ onMounted(() => {
   const docHeadObj = document.getElementsByTagName('head')[0];
   const jqueryScript = document.createElement('script');
   jqueryScript.src = 'https://cdn.getreplybox.com/js/embed.js';
-  jqueryScript.defer = true;
+  jqueryScript.defer = false;
   docHeadObj.appendChild(jqueryScript);
 });
 </script>
